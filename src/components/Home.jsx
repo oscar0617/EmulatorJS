a067d778131b740d5d2c8867c3c7e237c5cbf742
Oscar Lesmes <oscarsantiagolesmes@gmail.com>
2025-08-13 21:08:13 -0500

    aaa

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,9 +7,6 @@ const Home = () => {
       <h1>A Anthony lo toca el tio</h1>
       <div className='degenerados'>
         <img src="/degenerado1.png"/>
-        <img src="/degenerado2.png"/>
-        <img src="/degenerado3.png"/>
-        <img src="/degenerado4.png"/>
       </div>
     </div>
   );
